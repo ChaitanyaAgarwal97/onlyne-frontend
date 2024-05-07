c231a4f5746f04b07542efb961ec606966c6fe27
ChaitanyaAgarwal97 <chaitanyaagarwal97@gmail.com>
2024-05-07 17:54:24 +0530

    Font changed

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,7 +5,8 @@ import { ClerkProvider } from "@clerk/nextjs";
 
 import { ThemeProvider } from "@/components/providers/theme-provider";
 import { cn } from "@/lib/utils";
-const inter = Inter({ subsets: ["latin"] });
+
+const font = Open_Sans({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "OnLyne | Team Collaboration Tool",
@@ -20,16 +21,16 @@ export default function RootLayout({
   return (
     <ClerkProvider>
       <html lang="en" suppressHydrationWarning>
-        <body className={cn(inter.className, "bg-white dark:bg-[#313338]")}>
-          <ThemeProvider 
-          attribute="class"
-          defaultTheme="dark"
-          enableSystem={false}
-          storageKey="Onlyne-theme"
+        <body className={cn(font.className, "bg-white dark:bg-[#313338]")}>
+          <ThemeProvider
+            attribute="class"
+            defaultTheme="dark"
+            enableSystem={false}
+            storageKey="Onlyne-theme"
           >
             {children}
           </ThemeProvider>
-          </body>
+        </body>
       </html>
     </ClerkProvider>
   );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,8 @@
 
 import { ModeToggle } from "@/components/providers/mode-toggle";
-import Image from "next/image";
 
 export default function Home() {
   return (
-    <ModeToggle />  
+    <ModeToggle />
   );
 }
